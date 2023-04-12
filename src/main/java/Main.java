48c449130733d74de13ed50398ac95673beb110b
Yannick van Vulpen <yannick.graal@gmail.com>
2023-04-12 13:34:16 +0200

    Klant en klanttypes code van Chimene, NieuweKlantType en Admin code van Yannick programma runt tot schipkeuze OPNIEUW

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,5 @@
-import java.io.*;
-import java.util.*;
-import java.text.NumberFormat;
-
+import java.io.IOException;
+import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) throws IOException {
@@ -9,5 +7,90 @@ class Main {
         Startup startup = new Startup();
         banner.printBannerLogo();
         startup.askAdmin();
+        printKlantOpties();
+        printSchipOpties();
+    }
+
+    public static void printKlantOpties() {
+        Scanner scanner = new Scanner(System.in);
+        String naam, achternaam, email;
+        int telefoonNummer;
+        String bedrijf, overheidNaam;
+        System.out.printf("Selecteer klanttype: %n1: Particulier%n2: Zakelijk%n3: Overheid%n4: %s %n", NieuwKlantType.getNieuwKlantTypeNaam());
+
+        switch (scanner.nextLine()) {
+            case "1":
+                System.out.println("Voer uw voornaam in:");
+                naam = scanner.nextLine();
+                System.out.println("Voer uw achternaam in:");
+                achternaam = scanner.nextLine();
+                System.out.println("Voer uw emailadres in:");
+                email = scanner.nextLine();
+                System.out.println("Voer uw telefoonnummer in:");
+                telefoonNummer = scanner.nextInt();
+                Particulier particulier = new Particulier(naam, achternaam, email, telefoonNummer, null);
+                break;
+            case "2":
+                System.out.println("Voer uw voornaam in:");
+                naam = scanner.nextLine();
+                System.out.println("Voer uw achternaam in:");
+                achternaam = scanner.nextLine();
+                System.out.println("Voer uw emailadres in:");
+                email = scanner.nextLine();
+                System.out.println("Voer uw telefoonnummer in:");
+                telefoonNummer = scanner.nextInt();
+                System.out.println("Voer de naam van uw bedrijf in:");
+                scanner.nextLine();
+                bedrijf = scanner.nextLine();
+                Zakelijk zakelijk = new Zakelijk(naam, achternaam, email, telefoonNummer, bedrijf, null);
+                break;
+            case "3":
+                System.out.println("Voer uw voornaam in:");
+                naam = scanner.nextLine();
+                System.out.println("Voer uw achternaam in:");
+                achternaam = scanner.nextLine();
+                System.out.println("Voer uw emailadres in:");
+                email = scanner.nextLine();
+                System.out.println("Voer uw telefoonnummer in:");
+                telefoonNummer = scanner.nextInt();
+                System.out.println("Voer de naam van uw overheid in:");
+                scanner.nextLine();
+                overheidNaam = scanner.nextLine();
+                Overheid overheid = new Overheid(naam, achternaam, email, telefoonNummer, overheidNaam, null);
+                break;
+            case "4":
+                if (NieuwKlantType.getNieuwKlantTypeBestaat()) {
+                    System.out.printf("Welkom %s, voer uw voornaam in:", NieuwKlantType.getNieuwKlantTypeNaam());
+                    naam = scanner.nextLine();
+                    System.out.println("Voer uw achternaam in:");
+                    achternaam = scanner.nextLine();
+                    System.out.println("Voer uw emailadres in:");
+                    email = scanner.nextLine();
+                    System.out.println("Voer uw telefoonnummer in:");
+                    telefoonNummer = scanner.nextInt();
+                    NieuwKlantType customKlantType = new NieuwKlantType(naam, achternaam, email, telefoonNummer, null);
+                } else {
+                    System.out.println("Er bestaat nog geen Custom KlantType. Als u admin privileges hebt kunt u Custom KlantTypes aanmaken in het admin menu.");
+                    printKlantOpties();
+                }
+                break;
+            default:
+                System.out.println("Foutmelding. Voer een geldig antwoord in.");
+                printKlantOpties();
+        }
+    }
+    private static void printSchipOpties(){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.printf("Selecteer schiptype:%n1: SpeedBoot%n");
+        if(scanner.nextLine().equals("1")){
+            //SpeedBoot speedBoot = new SpeedBoot(null, null);
+            printSpeedbootOpties();
+        }
+    }
+
+    private static void printSpeedbootOpties(){
+        System.out.println("Kies essentiele opties: ");
+
     }
 }
